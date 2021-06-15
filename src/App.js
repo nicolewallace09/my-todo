ae796acc51b92db3e602521c961bcf6f70a7e263
nicolewallace09 <nicole.elisaw@gmail.com>
2021-06-14 22:06:45 -0700

    Playing around with colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'; 
 import TodoItem from './components/TodoItem'; 
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <>
+    <Header/> 
+    
     <div className="todo-list">
       <TodoItem/>
       <TodoItem/>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+
+function Header() {
+  return (
+    <>
+        <header>
+            <img src="https://img.icons8.com/fluent/20/000000/microsoft-todo-2019.png" alt="to-do-icon"/> To Do List
+        </header>
+  
+    </>
+  );
+}
+
+export default Header;
